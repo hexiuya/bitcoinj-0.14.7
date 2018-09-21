27e86fe7655f3a4bc7d30ddda2b3eb3a82f81730
hexiuya <hexiuya0902@hotmail.com>
2018-09-21 15:16:28 +0800

    sh script

--- bitcoin-wallet/run.sh
+++ bitcoin-wallet/run.sh
@@ -1,2 +1,2 @@
 Xvfb -ac :7 -screen 0 1024x768x16 &> xvfb.log &
-java -jar bitcoin-wallet-0.14.7.jar 
+java -jar -Dspring.config.location=application.properties bitcoin-wallet-0.14.7.jar 
---
+++ bitcoin-wallet/runOnJenkins.sh
@@ -0,0 +1,5 @@
+docker stop bitcoin-wallet
+docker rm bitcoin-wallet
+docker image rm bitcoin-wallet:0.0.1
+docker build ./bitcoin-wallet -t bitcoin-wallet:0.0.1
+docker run -d -p 8882:8882 --name bitcoin-wallet --network crm-network --network-alias alias-bitcoin-wallet --link crm-test-mysql bitcoin-wallet:0.0.1

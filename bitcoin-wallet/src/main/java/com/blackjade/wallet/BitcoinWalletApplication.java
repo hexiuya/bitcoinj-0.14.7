504e272c0468bdbe477c681c5d0c9c9826636f43
hexiuya <hexiuya0902@hotmail.com>
2018-09-26 16:39:59 +0800

    change dir

--- bitcoin-wallet/src/main/java/com/blackjade/wallet/BitcoinWalletApplication.java
+++ bitcoin-wallet/src/main/java/com/blackjade/wallet/BitcoinWalletApplication.java
@@ -110,7 +110,7 @@ public class BitcoinWalletApplication extends Application {
 
 	public void setupWalletKit(@Nullable DeterministicSeed seed) {
 		// If seed is non-null it means we are restoring from backup.
-		bitcoin = new WalletAppKit(params, new File("."), WALLET_FILE_NAME) {
+		bitcoin = new WalletAppKit(params, new File("./data/"), WALLET_FILE_NAME) {
 			@Override
 			protected void onSetupCompleted() {
 				// Don't make the user wait for confirmations for now, as the intention is they're sending it
